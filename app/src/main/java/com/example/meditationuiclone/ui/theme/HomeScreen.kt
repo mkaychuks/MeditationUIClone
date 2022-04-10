2b4d3fc1290b0b278eaa04627814c99aff4bfb2e
mkaychuks <sachufusi@gmail.com>
2022-04-10 18:44:24 +0100

    Implemented the greeting section

---
+++ app/src/main/java/com/example/meditationuiclone/ui/theme/HomeScreen.kt
@@ -0,0 +1,63 @@
+package com.example.meditationuiclone.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.meditationuiclone.R
+
+
+// the main homescreen with differenct composables
+@Composable
+fun HomeScreen(){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(DeepBlue)
+    ){
+        Column {
+            GreetingSection()
+        }
+    }
+}
+
+
+
+// greeting section composable
+@Composable
+fun GreetingSection(
+    name: String = "Ifeanyi"
+){
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good Morning $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day!",
+                style = MaterialTheme.typography.body1
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
